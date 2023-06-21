1d42be03f8b2b66ca5972c1fdb58555abf005590
Jamshid-DotNet <39515083+Jamshid999@users.noreply.github.com>
2023-06-21 23:52:15 +0500

    added some services

--- WeatherTelegramBot/Services/UpdateHandler.Message.cs
+++ WeatherTelegramBot/Services/UpdateHandler.Message.cs
@@ -1,15 +1,32 @@
 ﻿using Telegram.Bot.Types;
 using Telegram.Bot;
 using Serilog;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace WeatherTelegramBot.Services;
 
 public partial class UpdateHandler
 {
-    private Task HandleMessageUpdateAsync(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
+    private async Task HandleMessageUpdateAsync(ITelegramBotClient botClient, Message message, CancellationToken cancellationToken)
     {
         var username = message.From?.Username ?? message?.From?.FirstName;
         Log.Information("Received message from {username}", username);
-        return Task.CompletedTask;  
+
+        if(message.Text == "/start")
+        {
+            var sentMessage = await botClient.SendTextMessageAsync(
+                chatId:message.Chat.Id,
+                text:"Enter location!",
+                replyMarkup: new ReplyKeyboardMarkup(new KeyboardButton("Location")
+                {
+                    RequestLocation = true
+                }),cancellationToken:cancellationToken
+                );
+
+
+
+        }
+
+     
     }
 }
---
+++ WeatherTelegramBot/Services/WeatherService.cs
@@ -0,0 +1,37 @@
+﻿using BetterConsoles.Tables;
+using WeatherTelegramBot.Models;
+
+namespace WeatherTelegramBot.Services;
+
+public class WeatherService
+{
+    private readonly HttpClient _openMeteoClient;
+
+    public WeatherService(IHttpClientFactory openMeteoClient)
+    {
+        _openMeteoClient = openMeteoClient.CreateClient("OpenMeteoClient");
+    }
+
+    public async Task<string> GetWeatherStringAsync(double latitude, double longitude,CancellationToken cancellationToken = default)
+    {
+        var route = $"v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true";
+        var weather = await _openMeteoClient.GetFromJsonAsync<TheWeather>(route,cancellationToken);
+
+
+        var emojies = weather?.Temperature switch
+        {
+            >= 50 => "🔥",
+            >= 30 => "☀️",
+            >= 5 => "❄️",
+            _ => "🥶"
+
+
+        };
+        Table table = new Table("Weather now!");
+        table.AddRow($"{weather.Temperature:F1} {emojies}");
+        table.AddRow($"{weather.Windspeed} km/s");
+
+        return table.ToString();
+    }
+
+}
